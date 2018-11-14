17a4a0a0cd4c3ebcc1a3c313572bb2fec3a33f04
Anna Bagriy <>
2018-11-14 18:41:30 +0200

    Added new picture uploading view model with basic properties and view file without any functionality or views.

---
+++ ImageGallery/ImageGallery/ViewModels/UploadNewPicturePageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using ImageGallery.Core.Infrastructure;
+using Prism.Navigation;
+using Prism.Services;
+
+namespace ImageGallery.ViewModels
+{
+    public class UploadNewPicturePageViewModel : ViewModelBase
+    {
+        private Stream _image;
+        public Stream Image
+        {
+            get => _image;
+            set => SetProperty(ref _image, value);
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
+        private string _hashtag;
+        public string Hashtag
+        {
+            get => _hashtag;
+            set => SetProperty(ref _hashtag, value);
+        }
+
+        private float _longtitude;
+        public float Longtitude
+        {
+            get => _longtitude;
+            set => SetProperty(ref _longtitude, value);
+        }
+
+        private float _latitude;
+        public float Latitude
+        {
+            get => _latitude;
+            set => SetProperty(ref _latitude, value);
+        }
+
+        public UploadNewPicturePageViewModel(INavigationService navigationService, 
+                                             IPageDialogService dialogService) : base(navigationService, dialogService)
+        {
+        }
+    }
+}
